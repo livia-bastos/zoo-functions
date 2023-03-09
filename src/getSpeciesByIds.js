e77a8a9c5f58f958c72316360e0054334f86cbd2
Livia Bastos <livialbastos@gmail.com>
2023-03-09 17:21:13 -0300

    requisito1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const idSpecies = [];
+  ids.forEach((id) => {
+    const findSpecie = species.find((specie) => specie.id === id);
+    idSpecies.push(findSpecie);
+  });
+  return idSpecies;
 };
-
 module.exports = getSpeciesByIds;
