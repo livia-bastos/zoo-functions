88fd1052e6d70f4a4a937ba5e464ca830fbf6b7f
Livia Bastos <livialbastos@gmail.com>
2023-03-09 17:39:28 -0300

    erro lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,17 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => {
-  const idSpecies = [];
-  ids.forEach((id) => {
-    const findSpecie = species.find((specie) => specie.id === id);
-    idSpecies.push(findSpecie);
-  });
-  return idSpecies;
-};
+// const getSpeciesByIds = (...ids) => {
+//   const idSpecies = [];
+//   ids.forEach((id) => {
+//     const findSpecie = species.find((specie) => specie.id === id);
+//     idSpecies.push(findSpecie);
+//   });
+//   return idSpecies;
+// };
+// module.exports = getSpeciesByIds;
+
+const getSpeciesByIds = (...ids) => ids.map((id) => species
+  .find((specie) => specie.id === id));
+
 module.exports = getSpeciesByIds;
