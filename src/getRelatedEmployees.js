719a9a21ac9afa72ba765e05b1013603093af128
Livia Bastos <livialbastos@gmail.com>
2023-03-09 20:37:59 -0300

    requisito4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const comandado = employees.filter((employee) => employee.managers.includes(id));
+  if (comandado.length === 0) {
+    return false;
+  }
+  return true;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const comandado = employees.filter((employee) => employee.managers.includes(managerId));
+  return comandado.map((peao) => `${peao.firstName} ${peao.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
