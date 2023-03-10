a5cdfb7b2b21b7c59bf60c4aa2da3ed48d6be07b
Livia Bastos <livialbastos@gmail.com>
2023-03-10 15:18:54 -0300

    requisito 6

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const animals = () => species.reduce((acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+}, {});
 const countAnimals = (animal) => {
-  // seu código aqui
-};
 
+  const handlerElephants = (param) => {
+    if (animal === undefined) {
+      return animals();
+    }
+    const elephants = getElephants();
+    if (Object.keys(elephants).includes(param)) {
+      return elephants[param];
+  }
+  return computeData(param, elephants);
+};
+}
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,57 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('se chamar a função com um parametro especifico, deve retornar o resultado correspondente ao parametro passado', () => {
+    {
+      const actual = handlerElephants('count');
+      const expected = 4;
+      expect(actual).toEqual(expected);
+    }
+
+    {
+      const actual = handlerElephants('names');
+      const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+      expect(actual).toEqual(expected);
+    }
+    {
+      const actual = handlerElephants('averageAge');
+      const expected = 10.5;
+      expect(actual).toEqual(expected);
+    }
+  });
+  it('testa resultados para o param', () => {
+    {
+      const actual = handlerElephants();
+      const expected = undefined;
+      expect(actual).toEqual(expected);
+    }
+
+    {
+      const actual = handlerElephants('elephants');
+      const expected = null;
+      expect(actual).toEqual(expected);
+    }
+    {
+      const actual = handlerElephants('averageAge');
+      const expected = 10.5;
+      expect(actual).toEqual(expected);
+    }
+    // {
+    //   const actual = getElephants();
+    //   const expected = {
+    //     id: 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+    //     name: 'elephants',
+    //     popularity: 5,
+    //     location: 'NW',
+    //     availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
+    //     residents: [
+    //       { name: 'Ilana', sex: 'female', age: 11 },
+    //       { name: 'Orval', sex: 'male', age: 15 },
+    //       { name: 'Bea', sex: 'female', age: 12 },
+    //       { name: 'Jefferson', sex: 'male', age: 4 },
+    //     ],
+    //   };
+    //   expect(actual).toEqual(expected);
+    // }
+  });
+});
