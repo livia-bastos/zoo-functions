ea508af0b853eeb869a119b7c69021433678c90e
Livia Bastos <livialbastos@gmail.com>
2023-03-10 15:43:25 -0300

    lint requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,14 +1,14 @@
-const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
-const animals = () => species.reduce((acc, specie) => {
-  acc[specie.name] = specie.residents.length;
-  return acc;
-}, {});
-const countAnimals = (animal) => {
-  const countAnimals = () => {
-    if (animal === undefined) {
-      return animal();
-    }
-  };
-};
+// const animals = () => species.reduce((acc, specie) => {
+//   acc[specie.name] = specie.residents.length;
+//   return acc;
+// }, {});
+// const countAnimals = (animal) => {
+//   const countAnimals = () => {
+//     if (animal === undefined) {
+//       return animal();
+//     }
+//   };
+// };
