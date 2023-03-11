bdc3f61271c7ec120ab4b1d7cf669858bdb41bed
Livia Bastos <livialbastos@gmail.com>
2023-03-11 16:28:30 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const deMenor = entrants.filter((entrant) => entrant.age < 18).length;
+  const deMaior = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const velhos = entrants.filter((entrant) => entrant.age >= 50).length;
+  return {
+    child: deMenor,
+    adult: deMaior,
+    senior: velhos,
+  };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+
+  const somaAdulto = data.prices.adult * countEntrants(entrants).adult;
+  const somachild = data.prices.child * countEntrants(entrants).child;
+  const somaSenior = data.prices.senior * countEntrants(entrants).senior;
+
+  return (somaAdulto + somaSenior + somachild);
 };
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -18,6 +18,12 @@ describe('Testes da função HandlerElephants', () => {
       const expected = 10.5;
       expect(actual).toEqual(expected);
     }
+
+    {
+      const actual = handlerElephants(1);
+      const expected = 'Parâmetro inválido, é necessário uma string';
+      expect(actual).toEqual(expected);
+    }
   });
   it('testa resultados para o param', () => {
     {
@@ -36,22 +42,10 @@ describe('Testes da função HandlerElephants', () => {
       const expected = 10.5;
       expect(actual).toEqual(expected);
     }
-    // {
-    //   const actual = getElephants();
-    //   const expected = {
-    //     id: 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-    //     name: 'elephants',
-    //     popularity: 5,
-    //     location: 'NW',
-    //     availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
-    //     residents: [
-    //       { name: 'Ilana', sex: 'female', age: 11 },
-    //       { name: 'Orval', sex: 'male', age: 15 },
-    //       { name: 'Bea', sex: 'female', age: 12 },
-    //       { name: 'Jefferson', sex: 'male', age: 4 },
-    //     ],
-    //   };
-    //   expect(actual).toEqual(expected);
-    // }
+    {
+      const actual = handlerElephants('name');
+      const expected = 'elephants';
+      expect(actual).toEqual(expected);
+    }
   });
 });
