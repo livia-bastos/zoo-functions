5027779b1d7be68fb805973dbe8603ea056aa8a2
Livia Bastos <livialbastos@gmail.com>
2023-03-12 18:32:11 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const idEmployee = employees.find((employee) => employee.id === id);
+  const idAnimal = idEmployee.responsibleFor[0];
+  const { residents } = species.find((specie) => specie.id === idAnimal);
+  const findOlderAnimal = residents.sort((a, b) => b.age - a.age);
+  return Object.values(findOlderAnimal[0]);
 };
 
 module.exports = getOldestFromFirstSpecies;
