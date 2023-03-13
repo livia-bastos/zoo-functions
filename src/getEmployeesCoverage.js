aa560f6e975a1415d7a406d71b77cd0f3f415dfe
Livia Bastos <livialbastos@gmail.com>
2023-03-13 15:09:01 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,35 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+const getEmployeeByName = require('./getEmployeeByName');
+const getSpeciesByIds = require('./getSpeciesByIds');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const formataFuncao = (employee) => {
+  if (employee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const responsibleFor = getSpeciesByIds(...employee.responsibleFor);
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: responsibleFor.map((specie) => specie.name),
+    locations: responsibleFor.map((specie) => specie.location),
+  };
 };
+
+const getEmployeesCoverage = (nameorid) => {
+  if (nameorid === undefined) {
+    return employees.map((colaborador) => formataFuncao(colaborador));
+  }
+  let employee = {};
+  if (nameorid.name) {
+    employee = getEmployeeByName(nameorid.name);
+  }
+  if (nameorid.id) {
+    employee = employees.find((pessoa) => pessoa.id === nameorid.id);
+  }
+  return formataFuncao(employee);
+};
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
